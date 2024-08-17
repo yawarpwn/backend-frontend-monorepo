83ed73c8b4a259093bc8fcf7bd97e069b8cc083d
yawarpwn <john.kazz@gmail.com>
2024-08-16 19:25:19 -0500

    update server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,8 +1,24 @@
 import express from "express";
+import cors from "cors";
+import fileupload from "express-fileupload";
 
 const app = express();
 
+app.use(cors());
+app.use(fileupload());
+
 const PORT = 300;
+
+app.get("/", (req, res) => {
+  res.send("Server ON");
+});
+
+app.post("/api/upload", (req, res) => {
+  res.json({
+    success: true,
+    message: "File uploaded",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
