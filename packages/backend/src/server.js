c810a6e7a38fafc6aee25f00bfc32d288aca72bf
yawarpwn <john.kazz@gmail.com>
2024-08-16 19:59:20 -0500

    add filepond

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload";
 
 const app = express();
+
+app.use(cors());
+app.use(fileUpload());
 
 const PORT = 3000;
 
 app.use("/", (req, res) => {
   res.send("Server ON");
+});
+
+app.post("/api/upload", (req, res) => {
+  res.status({
+    status: "success",
+    message: "File uploaded",
+  });
 });
 
 app.listen(PORT, () => {
